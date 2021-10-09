38a402b1a292a16796bb2e00f159d823c0c4ce73
Wanderson Pinto <wanpdsantos@gmail.com>
2021-10-09 13:07:50 -0300

    Deployed Fuel Action endpoint

--- solution/carMaintenance/djangoBackend/challenge/models.py
+++ solution/carMaintenance/djangoBackend/challenge/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 class Car (models.Model):
-  totalGas = models.FloatField(default = 0, blank = False, null = False)
-  currentGas = models.FloatField(default = 0, blank = False, null = False)
+  totalGas = models.FloatField(default = 45, blank = False, null = False)
+  currentGas = models.FloatField(default = 20, blank = False, null = False)
 
   def __str__(self):
     return '{} - {} - {}'.format(self.id, self.totalGas, self.currentGas)
+
+@receiver(pre_save, sender=Car)
+def checkRequirements(sender, instance, update_fields, *args, **kwargs):
+  if not instance._state.adding:
+    currentGasPercentage = Car.objects.get(pk=instance.id).currentGas/Car.objects.get(pk=instance.id).totalGas
+    if (currentGasPercentage>0.05):
+      raise Exception('Current Gas above 5%.')
+  return
 
 class Tyre (models.Model):
   car = models.ForeignKey(Car, related_name='tyres', on_delete=models.CASCADE, blank=False, null=False)
--- solution/carMaintenance/djangoBackend/challenge/views.py
+++ solution/carMaintenance/djangoBackend/challenge/views.py
@@ -7,28 +7,41 @@ from .serializers import CarSerializer, TyreSerializer
 from .models import Car, Tyre
 
 class CarEventsViewset(viewsets.ViewSet):
-  
-  def list(self, request):
+
+  def list(self, request, *args, **kwargs):
     return Response({}, status=status.HTTP_501_NOT_IMPLEMENTED)
 
-  def retrieve(self, request, pk=None):
+  def retrieve(self, request, pk=None, *args, **kwargs):
     queryset = Car.objects.all()
     car = get_object_or_404(queryset, pk=pk)
     serializer = CarSerializer(car)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
-  def create(self, request):
+  def create(self, request, *args, **kwargs):
     newCar = Car.objects.create()
     serializer = CarSerializer(newCar)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
 
   @action(detail=True, methods=['post'])
-  def tyres(self, request, pk=None):
+  def tyres(self, request, pk=None, *args, **kwargs):
     try:
       newTyre = Tyre.objects.create(car_id=pk)
       serializer = TyreSerializer(newTyre)
       return Response(serializer.data, status=status.HTTP_201_CREATED)
     except Exception as e:
       return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+  @action(detail=True, methods=['patch'])
+  def fuel(self, request, pk=None, *args, **kwargs):
+    try:
+      queryset = Car.objects.all()
+      car = get_object_or_404(queryset, pk=pk)
+      car.currentGas = car.currentGas + float(request.data['quantity'])
+      car.save(update_fields=['currentGas'])
+      return Response({'currentGas': '{}%'.format(round((car.currentGas/car.totalGas)*100,2))}, status=status.HTTP_202_ACCEPTED)
+    except Exception as e:
+      return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
 
 
